6353c1e510e534ac4d5bae700c4b1ebeaef954b1
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-12-04 23:49:04 -0300

    Calculadora ok. Ainda implementar função para salvar ult. op. no local storage

--- javaScript.js
+++ javaScript.js
@@ -1,1 +1,22 @@
-// CRIANDO REPOSITÓRIO
+function insert(num){
+
+    let numero = document.getElementById("current").innerHTML
+    document.getElementById('current').innerHTML = numero + num
+
+}
+
+function calc() {
+    
+    let resultado = document.getElementById("current").innerHTML
+
+    if(resultado){
+        document.getElementById("current").innerHTML = eval(resultado)
+        document.getElementById("last-result").innerHTML = eval(resultado)
+    }
+    else{
+        document.getElementById("current").innerHTML = 'Nada para calcular'
+    }
+
+}
+
+
